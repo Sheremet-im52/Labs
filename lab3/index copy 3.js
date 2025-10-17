7ebd65b635bc161843928c22ea37586d611fd1ed
Влад Шеремет <_@example.com>
2025-10-17 21:32:28 +0300

    ...

---
+++ lab3/index copy 3.js
@@ -0,0 +1,20 @@
+function introspect(iface) {
+  const result = [];
+
+  for (const key in iface) {
+    const value = iface[key];
+
+    
+    if (typeof value === 'function') { 
+      result.push([key, value.length]);
+    }
+  }
+
+  return result;
+}
+const iface = {
+  m1: x => [x],
+  m2: function (x, y) { return [x, y]; },
+  m3(x, y, z) { return [x, y, z]; }
+};
+console.log(introspect(iface));
---
+++ lab3/index copy.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function random(min, max) {
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateKey(length, characters) {
+  let key = ''; 
+
+  for (let i = 0; i < length; i++) {
+   
+    const randomIndex = random(0, characters.length - 1);
+   
+    key += characters[randomIndex];
+  }
+
+  return key;
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key); 
